03cf5fce4c88b1ca6b1d63ccc508cb6677972ee3
kamal <shivamkamal.9936@gmail.com>
2026-04-15 13:50:34 +0530

    update

--- Backend/routes/appointmentRoutes.js
+++ Backend/routes/appointmentRoutes.js
@@ -1,58 +1,23 @@
 const express = require("express");
 const router = express.Router();
-const { bookAppointment } = require("../controllers/appointmentController");
-const Appointment = require("../models/appointment");
-const { nextPatient } = require("../controllers/appointmentController");
+const {
+  bookAppointment,
+  nextPatient,
+  getUserAppointments,
+  cancelAppointment,
+  getDoctorAppointments,
+  getQueueStatus,
+  markEmergency,
+} = require("../controllers/appointmentController");
+
 
 router.post("/book", bookAppointment);
+router.get("/user/:userId", getUserAppointments);
+router.get("/doctor/:doctorId", getDoctorAppointments);
+router.get("/queue/:doctorId/:patientId", getQueueStatus);
+router.post("/next/:doctorId", nextPatient);
+router.patch("/emergency/:id", markEmergency);
+router.delete("/:id", cancelAppointment);
 
-// Queue Status API
-router.get("/queue/:doctorId/:patientId", async (req, res) => {
-  try {
-    const { doctorId, patientId } = req.params;
-
-    // current serving patient
-    const current = await Appointment.findOne({
-      doctorId,
-      status: "serving"
-    });
-
-    // current user ka appointment
-    const userAppointment = await Appointment.findOne({
-      doctorId,
-      patientId
-    });
-
-    // waiting list
-    const waiting = await Appointment.find({
-      doctorId,
-      status: "waiting"
-    }).sort({ tokenNumber: 1 });
-
-    // kitne log aage hai
-    const patientsAhead = waiting.filter(
-      a => a.tokenNumber < userAppointment.tokenNumber
-    ).length;
-
-    const avgTime = 10; // abhi fix rakho
-
-    const waitingTime = patientsAhead * avgTime;
-
-    res.json({
-      currentToken: current?.tokenNumber || 0,
-      yourToken: userAppointment.tokenNumber,
-      patientsAhead,
-      waitingTime
-    });
-
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Error in queue API" });
-  }
-});
-
-// new route for doctor button 
-
-router.post("/queue/next", nextPatient);
 
 module.exports = router;
